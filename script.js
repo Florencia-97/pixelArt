ca8264348636e10403bcf748346377ae5d5555b8
Flox <florrr1997@gmail.com>
2018-10-20 12:43:59 -0300

    me quedo con un header

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-
-window.onload=function(){
-
-  var header = document.getElementById("firma");
-
-  header.addEventListener("mouseover", function( event ){
-    html = "";
-    if (header.innerHTML.includes("Flor")){
-      html = `<img class="dibujo" src="img/header.png" alt="Bienvenido">`;
-    }
-    else {
-      html = `<img class="dibujo" src="img/headerFlor.png" alt="Bienvenido">`;
-    }
-    header.innerHTML = html;
-  }, true);
-}
+// Nada por aquí aun
